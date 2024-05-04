657bca3b99b7653e0f2a935d9e11780ab71888dc
rasha <rasha_alshraim@hotmail.com>
2024-05-04 22:48:15 +0200

    add prep w1

---
+++ Week1/prep-exercises/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO categories (category_name) VALUES ('Japanese');
+INSERT INTO categories (category_name) VALUES ('Cake');
+INSERT INTO categories (category_name) VALUES ('Vegetarian');
+INSERT INTO categories (category_name) VALUES ('Italian');
+INSERT INTO categories (category_name) VALUES ('Mexican');
+
+INSERT INTO ingredients (ingredient_name, measurement_unit, amount) VALUES ('Sake', 'tbsp', 3);
+INSERT INTO ingredients (ingredient_name, measurement_unit, amount) VALUES ('cakeMix', 'grams', 200);
+INSERT INTO ingredients (ingredient_name, measurement_unit, amount) VALUES ('Tofu', 'grams', 300);
+INSERT INTO ingredients (ingredient_name, measurement_unit, amount) VALUES ('Pasta', 'grams', 400);
+INSERT INTO ingredients (ingredient_name, measurement_unit, amount) VALUES ('mushroom', 'count', 2);
+
+INSERT INTO steps (step_name, step_description) VALUES ('add sake', ' pour in the sake to create a thicker sauce');
+INSERT INTO steps (step_name, step_description) VALUES ('preparing cakeMix', 'Use Butter Instead of Oil , Add an Extra Egg');
+INSERT INTO steps (step_name, step_description) VALUES ('preparing Pasta', 'cook pasta according to the package instructions.');
